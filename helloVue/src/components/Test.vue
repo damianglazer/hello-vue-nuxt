c235d4e74ad9457ab40f5f41ea369fd1fcc6e2a2
Damian Glazer <d.glazer@ideo.pl>
2022-09-27 09:07:31 +0200

    feat: first day

---
+++ helloVue/src/components/Test.vue
@@ -0,0 +1,84 @@
+<script setup>
+  import {ref, onBeforeMount} from 'vue';
+  import axios from 'axios';
+
+  const blur = ref(1);
+  const width = ref(200);
+  const height = ref(200);
+  const page = ref(1);
+  const imagesList = ref([]);
+
+  const check = () => {
+    blur.value
+  }
+
+  const getData = async (pageNumber = 1) => {
+    if(pageNumber <= 0) {
+      return;
+    }
+
+    const { data } = await axios(`https://picsum.photos/v2/list?page=${pageNumber}`);
+
+    imagesList.value = data;
+    page.value = pageNumber
+
+    return data;
+  }
+
+  onBeforeMount(() => {
+    getData();
+  });
+</script>
+
+<template>
+  <div>
+    Test {{blur}}
+    <button @click="blur++">Dodaj</button>
+    <button @click="blur--">Odejmij</button>
+    <img alt="Vue logo" class="logo" :src="`https://picsum.photos/id/40/${width}/${height}${blur > 0 ? '?blur='+blur : ''}`" />
+  
+  {{`https://picsum.photos/id/40/${width}/${height}${blur > 0 ? '?blur='+blur : ''}`}}
+  <div>
+    <label>
+      <span>Wysokość</span>
+      <input type="number" v-model="width"/>
+    </label>
+    <hr />
+    <label>
+      <span>Szerokość</span>
+      <input type="number" v-model="height"/>
+    </label>
+  </div>
+  <button @click="check">Sprawdź</button>
+
+  <div class="list">
+      <div v-for="image in imagesList" class="img-container">
+          {{image.author}}
+          <img :src="`https://picsum.photos/id/${image.id}/${width}/${height}`" />
+      </div>
+  </div>
+
+  <div>
+    <button @click="getData(page - 1)">Poprzednia</button>
+    {{page}}
+    <button @click="getData(page + 1)">Następna</button>
+  </div>
+  <!-- <div>
+    <pre>{{imagesList}}</pre>
+  </div> -->
+  </div>
+</template>
+
+<style scoped>
+  .list {
+    display: flex;
+    flex-wrap: wrap;
+    width: 100%;
+  }
+  .img-container {
+    max-width: 20%;
+  }
+  img {
+    width: 100%;
+  }
+</style>
